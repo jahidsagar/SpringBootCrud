49fc9f4c7c46f0ec10124d70f94fb2b397384365
jahidsagar <jahidsagar@gmail.com>
2023-06-08 22:45:23 +0600

    Initial commit

---
+++ src/main/java/com/example/SpringBootRestInMemory/SpringBootRestInMemoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootRestInMemory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestInMemoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestInMemoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootRestInMemory/SpringBootRestInMemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootRestInMemory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestInMemoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
